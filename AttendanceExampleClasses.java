bd5f0cb89c893837b9d36de0fc04d3ee296424b7
Katalina_Moon <catt1235@gmail.com>
2025-02-14 09:34:33 -0500

    Add files via upload

---
+++ AttendanceExampleClasses.java
@@ -0,0 +1,22 @@
+public class AttendanceExampleClasses {
+   public static void main(String[] args) {
+      PeopleCounter attendeeCounter = new PeopleCounter();
+      
+      attendeeCounter.incrementCount();
+      attendeeCounter.incrementCount();
+      attendeeCounter.incrementCount();
+      
+      System.out.print("Attendee count: ");
+      attendeeCounter.printCount();
+   }
+}
+
+/*
+
+A class defines a type that groups data and methods to form an object. A class' 
+public methods define the operations that can be performed on objects of that class type. 
+The class definition (discussed later) is like a blueprint explaining how to create objects and an instruction manual explaining how to use an object.
+
+The below code creates an object attendeeCounter using a class named PeopleCounter, with relevant code highlighted below.
+
+*/
