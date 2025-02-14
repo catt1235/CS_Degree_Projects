eb9cc06f0491d4d0f47c7454f081fe327981207e
Katalina_Moon <catt1235@gmail.com>
2025-02-14 09:38:29 -0500

    Delete Arrays directory
    Too many small nonsense files.

--- Arrays/PrintingArraysWithForLoop.java
+++
@@ -1,9 +0,0 @@
-public class PrintingArraysWithForLoop {
-  public static void main(String[] args) {
-	int numbers[] = {1, 2, 3, 4, 5};
-	
-    for (int number : numbers) //for every integer number, in the numbers array, print.
-		System.out.println(number);
-    }
-  }
-}
